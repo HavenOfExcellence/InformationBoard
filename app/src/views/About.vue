48a78482aedc6d1a9007999387fd50ba29807e21
czyan <czyang2002@gmail,com>
2021-12-23 16:20:21 +0800

    working markdown generator

--- app/src/views/About.vue
+++ app/src/views/About.vue
@@ -1,13 +1,56 @@
 <template>
 <div>
-  <faq />
+  <faq :markdown="this.md"/>
 </div>
 </template>
 
 <script>
+const md = `# :sparkles: vue3-markdown-it demo :sparkles:
+
+> Welcome to the demo Feel free to type anything in the textarea!
+> > *Psst, enable the  option!* :P
+
+https://github.com/JanGuillermo/vue3-markdown-it (*Enable the option!~*)
+
+___
+
+# This is a h1 tag
+## This is a h2 tag
+### This is a h3 tag
+#### This is a h4 tag
+##### This is a h5 tag
+###### This is a h6 tag
+
+Enable the option to see the magic.
+
+---
+
+## Basic stuff
+
+## Lists
+
+Sometimes you want numbered lists:
+
+1. One
+2. Two
+3. Three
+
+Sometimes you want bullet points:
+
+* Start a line with a star
+* Profit!
+
+
+
+`
 import Faq from '../components/faq.vue'
 export default {
   name:"About",
-  components:{Faq}
+  components:{Faq},
+  data(){
+    return {
+      md:md
+    }
+  }
 }
 </script>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,15 +1,22 @@
 <template>
- <div class="bg-green-300 border-green-600 border-b p-4 m-4 rounded">
-  Hello World
+ <div v-on:click="gotoarticle()" class="transition-all duration-300 ease-linear bg-gray-800 text-green-500 hover:bg-green-600 hover:text-white p-4 m-4 rounded-xl relative flex items-center justify-center mt-2 mb-2 shadow-lg">
+  Losing your 11B
 </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
+
 export default defineComponent({
   name: "Home",
   components: {
-  },
+    },
+    methods: {
+      gotoarticle(){
+        
+        this.$router.push("/about");
+      }
+    }
 });
 </script>
