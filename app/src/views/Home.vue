1c7b6bbd8679356076ed200d168ccd0757a20d8a
czyan <czyang2002@gmail,com>
2021-12-23 11:09:24 +0800

    Vue3 + Tailwind Template

---
+++ app/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+ <div class="bg-green-300 border-green-600 border-b p-4 m-4 rounded">
+  Hello World
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+  },
+});
+</script>
